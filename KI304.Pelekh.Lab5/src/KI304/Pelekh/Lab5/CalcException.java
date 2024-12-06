ffcdfa89ffe1a94a152190d1b06ddc21150ad702
vasilko2005 <156843929+vasilko2005@users.noreply.github.com>
2024-12-06 21:27:44 +0200

    Add files via upload

---
+++ KI304.Pelekh.Lab5/src/KI304/Pelekh/Lab5/CalcException.java
@@ -0,0 +1,9 @@
+package KI304.Pelekh.Lab5;
+
+
+class CalcException extends ArithmeticException {
+    public CalcException() {}  
+    public CalcException(String cause) { 
+        super(cause); 
+    }
+}
