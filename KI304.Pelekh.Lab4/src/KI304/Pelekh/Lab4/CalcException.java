033ecf82da9b55510200872663eb83a238df9ee1
vasilko2005 <156843929+vasilko2005@users.noreply.github.com>
2024-12-06 21:22:10 +0200

    Add files via upload

---
+++ KI304.Pelekh.Lab4/src/KI304/Pelekh/Lab4/CalcException.java
@@ -0,0 +1,10 @@
+package KI304.Pelekh.Lab4;
+
+
+class CalcException extends ArithmeticException {
+    public CalcException() {}  
+    public CalcException(String cause) { 
+        super(cause); 
+    }
+}
+
