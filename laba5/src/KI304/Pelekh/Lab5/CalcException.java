21c8946ff45568177b509c8cbde8823ac3ed03f0
vasilko2005 <156843929+vasilko2005@users.noreply.github.com>
2024-12-06 21:17:26 +0200

    Delete laba5 directory

--- laba5/src/KI304/Pelekh/Lab5/CalcException.java
+++
@@ -1,9 +0,0 @@
-package KI304.Pelekh.Lab5;
-
-
-class CalcException extends ArithmeticException {
-    public CalcException() {}  
-    public CalcException(String cause) { 
-        super(cause); 
-    }
-}
